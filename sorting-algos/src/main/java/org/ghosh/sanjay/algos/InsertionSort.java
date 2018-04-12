379f33ee4198f693d234a5888b18f6f1a25ddeaf
codeWriter9 <nerd.subhankar.2006@gmail.com>
2018-04-12 19:58:16 +0530

    Creating a repository to hold some Sorting Algorithm Implementations

---
+++ sorting-algos/src/main/java/org/ghosh/sanjay/algos/InsertionSort.java
@@ -0,0 +1,17 @@
+package org.ghosh.sanjay.algos;
+
+import java.util.List;
+
+public class InsertionSort<T extends Comparable<T>> implements Sort<T>
+{
+	public void sort()
+	{
+		
+	}
+	
+	
+	public List<T> sorted()
+	{
+		return null;
+	}	
+}
---
+++ sorting-algos/src/main/java/org/ghosh/sanjay/algos/Sort.java
@@ -0,0 +1,23 @@
+package org.ghosh.sanjay.algos;
+
+import java.util.List;
+
+public interface Sort<T extends Comparable<T>> 
+{
+	/**
+	 *
+	 *
+	 *
+	 *
+	 **/
+	public void sort();
+	
+	/**
+	 *
+	 *
+	 *
+	 *
+	 **/
+	public List<T> sorted();
+		
+}
