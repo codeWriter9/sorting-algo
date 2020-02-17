138b6e8d2cc100e8e6d20eaebd47736d6c0ab902
Sanjay <Sanjay.X.Ghosh@gmail.com>
2020-02-17 23:04:28 +0530

    ADD - Generic Factory

---
+++ sorting-algos/src/main/java/org/ghosh/sanjay/algos/SortingAlgo.java
@@ -0,0 +1,7 @@
+package org.ghosh.sanjay.algos;
+
+public enum SortingAlgo {
+
+	INSERTION_SORT, MERGE_SORT, QUICK_SORT, SELECTION_SORT, SHELL_SORT
+
+}
---
+++ sorting-algos/src/main/java/org/ghosh/sanjay/algos/SortingFactories.java
@@ -0,0 +1,47 @@
+package org.ghosh.sanjay.algos;
+
+public final class SortingFactories {
+
+	public Sort<Integer> integerSort(SortingAlgo algo, Integer[] array) {
+		switch (algo) {
+		case INSERTION_SORT:
+			return new InsertionSort<>(array);
+		case MERGE_SORT:
+			return new MergeSort<>(array);
+		case QUICK_SORT:
+			return new QuickSort<>(array);
+		case SELECTION_SORT:
+			return new SelectionSort<>(array);
+		case SHELL_SORT:
+			return new ShellSort<>(array);
+		default:
+			throw new IllegalArgumentException(" algo not found ");
+		}
+	}
+
+	public Sort<Long> longSort(SortingAlgo algo, Long[] array) {
+		switch (algo) {
+		case INSERTION_SORT:
+			return new InsertionSort<>(array);
+		case MERGE_SORT:
+			return new MergeSort<>(array);
+		case QUICK_SORT:
+			return new QuickSort<>(array);
+		case SELECTION_SORT:
+			return new SelectionSort<>(array);
+		case SHELL_SORT:
+			return new ShellSort<>(array);
+		default:
+			throw new IllegalArgumentException(" algo not found ");
+		}
+	}
+
+	public <T> Sort<?> sort(SortingAlgo algo, T[] array) {
+		if (array instanceof Integer[])
+			return integerSort(algo, (Integer[]) array);
+		else if (array instanceof Long[])
+			return longSort(algo, (Long[]) array);
+		else
+			throw new IllegalArgumentException(" algo not found ");
+	}
+}
